992c0d453c5d89a3fd3fee8aa5a25f2a438818f8
tianyin <tianyin0910@emoney.cn>
2020-09-16 14:52:42 +0800

    udpate

--- HappyShop.Api/Startup.cs
+++ HappyShop.Api/Startup.cs
@@ -40,8 +40,6 @@ namespace HappyShop.Api
             services.AddHappyShopService();
             services.AddHappyShopData();
             services.AddControllers();
-            services.AddMvc();
-            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
