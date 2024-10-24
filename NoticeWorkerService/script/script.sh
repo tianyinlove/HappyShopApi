5f753c11f794965072a4f1472cef8fcc416de649
tianyin <tianyin0910@emoney.cn>
2024-10-24 09:23:59 +0800

    更新

--- NoticeWorkerService/script/script.sh
+++ NoticeWorkerService/script/script.sh
@@ -1,2 +1,5 @@
 ﻿#!/bin/bash
-curl -X POST https://alextian.tpddns.cn:4431/hpapi/MyFollow/SendMessage
+while true; do
+    curl -X GET "https://alextian.tpddns.cn:4431/hpapi/MyFollow/SendMessage"
+    sleep 1
+done
